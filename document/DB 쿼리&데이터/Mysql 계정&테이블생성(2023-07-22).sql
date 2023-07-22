870d7e14ec45d24d3220e2f8677858b27596d7cc
humi53 <j.k.humi9@gmail.com>
2023-07-22 20:30:24 +0900

    테이블명세, 생성쿼리 오류 수정

--- document/DB 쿼리&데이터/Mysql 계정&테이블생성(2023-07-22).sql
+++ document/DB 쿼리&데이터/Mysql 계정&테이블생성(2023-07-22).sql
@@ -17,6 +17,7 @@ FLUSH PRIVILEGES;
 use footprintDB;
 
 -- 행정구역
+DROP TABLE IF EXISTS tb_district;
 CREATE TABLE tb_district(
 dis_code	VARCHAR(4)		PRIMARY KEY,
 dis_name	VARCHAR(10)	NOT NULL,	
@@ -24,6 +25,7 @@ dis_deeplv	INT	NOT NULL
 );
 
 -- 하위행정구역
+DROP TABLE IF EXISTS tb_subdist;
 CREATE TABLE tb_subdist(
 sdis_code	VARCHAR(18)		PRIMARY KEY,
 sdis_name	VARCHAR(10)	NOT NULL,	
@@ -36,19 +38,21 @@ sdis_discode	VARCHAR(4)	NOT NULL
 -- ADD FOREIGN KEY (sdis_discode) REFERENCES tb_district(dis_code);
 
 -- api지도마커데이터
+DROP TABLE IF EXISTS tb_apimark;
 CREATE TABLE tb_apimark(
 mk_seq	BIGINT	AUTO_INCREMENT	PRIMARY KEY,
 mk_name	VARCHAR(30)	NOT NULL,	
 mk_lati	VARCHAR(14)	NOT NULL,	
 mk_longi	VARCHAR(14)	NOT NULL,	
 mk_type	INT	NOT NULL,	
-sdis_code2	VARCHAR(18)	NOT NULL	
+mk_sdiscode	VARCHAR(18)	NOT NULL	
 );
 --  ALTER TABLE tb_apimark
--- ADD FOREIGN KEY (sdis_code2) REFERENCES tb_subdist(sdis_code);
+-- ADD FOREIGN KEY (mk_sdiscode) REFERENCES tb_subdist(sdis_code);
 
 
 -- sns게시글
+DROP TABLE IF EXISTS tb_snspost;
 CREATE TABLE tb_snspost(
 sp_seq	BIGINT	AUTO_INCREMENT	PRIMARY KEY,
 sp_title	VARCHAR(30)	NOT NULL,	
@@ -56,16 +60,17 @@ sp_content	VARCHAR(400)	NOT NULL,
 sp_cdate	VARCHAR(10)	NOT NULL,	
 sp_mdate	VARCHAR(10)	NOT NULL,	
 sp_mkseq	BIGINT,		
-mb_seq	BIGINT		
+sp_mbseq	BIGINT		
 );
 -- ALTER TABLE tb_snspost
 -- ADD FOREIGN KEY (sp_mkseq) REFERENCES tb_apimark(mk_seq);
 -- ALTER TABLE tb_snspost
--- ADD FOREIGN KEY (mb_seq) REFERENCES tb_member(mb_seq);
+-- ADD FOREIGN KEY (sp_mbseq) REFERENCES tb_member(mb_seq);
 
 
 
 -- sns게시글이미지
+DROP TABLE IF EXISTS tb_snsimg;
 CREATE TABLE tb_snsimg(
 spi_seq	BIGINT	AUTO_INCREMENT	PRIMARY KEY,
 spi_uri	VARCHAR(100)	NOT NULL,	
@@ -78,8 +83,9 @@ spi_spseq	BIGINT	NOT NULL
 
 
 -- 하트
+DROP TABLE IF EXISTS tb_snsheart;
 CREATE TABLE tb_snsheart(
-sh_spseq	BIGINT,
+sh_spseq	BIGINT	NOT NULL,
 sh_mbseq	BIGINT	NOT NULL,
 PRIMARY KEY(sh_spseq,sh_mbseq)
 );
@@ -92,6 +98,7 @@ PRIMARY KEY(sh_spseq,sh_mbseq)
 
 
 -- 좋아하는 발가락
+DROP TABLE IF EXISTS tb_follow;
 CREATE TABLE tb_follow(
 fol_seq	BIGINT	AUTO_INCREMENT	PRIMARY KEY,
 followin	BIGINT	NOT NULL	NOT NULL,
@@ -107,6 +114,7 @@ follower	BIGINT	NOT NULL	NOT NULL
 
 
 -- 회원
+DROP TABLE IF EXISTS tb_member;
 CREATE TABLE tb_member(
 mb_seq	BIGINT	AUTO_INCREMENT	PRIMARY KEY,
 mb_id	VARCHAR(30)	NOT NULL,	
@@ -120,13 +128,14 @@ mb_email	VARCHAR(40)	NOT NULL,
 mb_addr	VARCHAR(200)		
 );
 -- sns코멘트
+DROP TABLE IF EXISTS tb_snscom;
 CREATE TABLE tb_snscom(
-sc_spseq	BIGINT,		
-sc_mbseq	BIGINT,	
+sc_spseq	BIGINT	NOT NULL,		
+sc_mbseq	BIGINT	NOT NULL,	
 sc_tnick	VARCHAR(10)	NOT NULL,
 sc_content	VARCHAR(200)	NOT NULL,	
 sc_cdate	VARCHAR(10)	NOT NULL,
-sc_mdate	VARCHAR(10)	NOT NULL,	
+sc_mdate	VARCHAR(10),	
 PRIMARY KEY(sc_spseq,sc_mbseq)
 );
 -- ALTER TABLE tb_snscom
@@ -137,8 +146,8 @@ PRIMARY KEY(sc_spseq,sc_mbseq)
 
 
 -- foreign key 삭제
-alter table tb_subdist
-drop FOREIGN KEY tb_subdist_ibfk_1;
+-- alter table tb_subdist
+-- drop FOREIGN KEY tb_subdist_ibfk_1;
 
 
 
